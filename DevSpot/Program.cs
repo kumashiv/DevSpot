53eb947dade4dc32a1664f4dd9f810592c5dd01b
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-02 16:56:23 -0400

    Seeding Roles
    add scope for roleManager
    Seed Admin role
    
    Refactor Seeder class
    Constants/Roles.cs
    DATA/RoleSeeder.cs class

--- DevSpot/Program.cs
+++ DevSpot/Program.cs
@@ -1,3 +1,4 @@
+using DevSpot.Constants;
 using DevSpot.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,13 @@ namespace DevSpot
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+            }
+
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                RoleSeeder.SeedRolesAsync(services).Wait();
             }
 
             app.UseHttpsRedirection();
