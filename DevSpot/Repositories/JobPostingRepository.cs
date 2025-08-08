d731912506da466265847ac68e233583ceb4c400
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-07 20:26:10 -0400

    JobPostingRepository - Implement logic
    - AddAsync
    - DeleteAsync
    - GetAllAsync
    - GetByIdAsync
    - UpdateAsync

--- DevSpot/Repositories/JobPostingRepository.cs
+++ DevSpot/Repositories/JobPostingRepository.cs
@@ -1,5 +1,6 @@
 ﻿using DevSpot.Data;
 using DevSpot.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevSpot.Repositories
 {
@@ -13,29 +14,45 @@ namespace DevSpot.Repositories
         }
 
 
-        public Task AddAsync(JobPosting entity)
+        public async Task AddAsync(JobPosting entity)
         {
-            throw new NotImplementedException();
+            await _context.JobPostings.AddAsync(entity);
+            await _context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var jobPosting = await _context.JobPostings.FindAsync(id);
+
+            if (jobPosting == null)
+            {
+                throw new NotImplementedException();
+            }
+            _context.JobPostings.Remove(jobPosting);
+            await _context.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<JobPosting>> GetAllAsync()
+        public async Task<IEnumerable<JobPosting>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            return await _context.JobPostings.ToListAsync();
         }
 
-        public Task<JobPosting> GetByIdAsync(int id)
+        public async Task<JobPosting> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            var jobPosting = await _context.JobPostings.FindAsync(id);
+
+            if (jobPosting == null)
+            {
+                throw new KeyNotFoundException();
+            }
+
+            return jobPosting;
         }
 
-        public Task UpdateAsync(JobPosting entity)
+        public async Task UpdateAsync(JobPosting entity)
         {
-            throw new NotImplementedException();
+            _context.JobPostings.Update(entity);
+            await _context.SaveChangesAsync();
         }
     }
 }
