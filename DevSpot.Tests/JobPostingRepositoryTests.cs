5770b46aefc069ab895f46329131383cfea72254
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-12 19:05:01 -0400

    Unit Tests DevSpot.Tests
    - Add Project reference
    - InMemoryDatabase
    - Create new instance of our in-memory database

---
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DevSpot.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevSpot.Tests
+{
+    internal class JobPostingRepositoryTests
+    {
+        private readonly DbContextOptions<ApplicationDbContext> _options;
+
+        public JobPostingRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase("JobPostingDb")
+                .Options;
+        }
+
+        //private ApplicationDbContext CreateDbContext() => new ApplicationDbContext(_options);
+        // or write this as
+        private ApplicationDbContext CreateDbContext()
+        {
+            return new ApplicationDbContext(_options);
+        }
+
+
+    }
+}
