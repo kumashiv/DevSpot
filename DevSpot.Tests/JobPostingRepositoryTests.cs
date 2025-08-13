8679242eb2056b8d3039713f5d1c19a3b2b2a8b5
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-12 22:23:18 -0400

    GeatAllAsync_ShouldReturnAllJobPostings

--- DevSpot.Tests/JobPostingRepositoryTests.cs
+++ DevSpot.Tests/JobPostingRepositoryTests.cs
@@ -98,5 +98,43 @@ namespace DevSpot.Tests
                 () => repository.GetByIdAsync(999)
             );
         }
+
+        [Fact]
+        public async Task GeatAllAsync_ShouldReturnAllJobPostings()
+        {
+            var db = CreateDbContext();
+
+            var repository = new JobPostingRepository(db);
+
+            var jobPosting1 = new JobPosting
+            {
+                Title = "Test Title 1",
+                Description = "Test Description 1",
+                PostedDate = DateTime.Now,
+                Company = "Test Company 1",
+                Location = "Test Location 1",
+                UserId = "TestUserId 1"
+            };
+
+            var jobPosting2 = new JobPosting
+            {
+                Title = "Test Title 2",
+                Description = "Test Description 2",
+                PostedDate = DateTime.Now,
+                Company = "Test Company 2",
+                Location = "Test Location 2",
+                UserId = "TestUserId 2"
+            };
+
+            await db.JobPostings.AddRangeAsync(jobPosting1,jobPosting2);
+
+            await db.SaveChangesAsync();
+
+            var result = await repository.GetAllAsync();
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+        }
+
     }
 }
