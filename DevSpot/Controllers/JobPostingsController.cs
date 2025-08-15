e1b70f3292011aef67b7fd77c7c538d24c07f00e
shiv kumar <kumarshiv_sk@outlook.com>
2025-08-14 21:51:09 -0400

    Job Postings Controller Create Method - Started
    JobPostings View Folder
    Create html page with form

---
+++ DevSpot/Controllers/JobPostingsController.cs
@@ -0,0 +1,41 @@
+﻿using DevSpot.Models;
+using DevSpot.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevSpot.Controllers
+{
+    public class JobPostingsController : Controller
+    {
+        private readonly IRepository<JobPosting> _repository;
+        private readonly UserManager<IdentityUser> _userManager;    // For user account related information
+
+        public JobPostingsController(
+            IRepository<JobPosting> repository,
+            UserManager<IdentityUser> userManager)
+        {
+            _repository = repository;
+            _userManager = userManager;
+        }
+
+
+        public async Task<IActionResult> Index()
+        {
+            var jobPostings = await _repository.GetAllAsync();  // GetAllAsync - from Repository
+            return View(jobPostings);
+        }
+
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(JobPosting jobPosting)
+        {
+            return RedirectToAction(nameof(Index));    // or simply ("Index")
+        }
+
+    }
+}
